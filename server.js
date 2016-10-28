441d94bdb4f2d5b37249bd8c3b990d04bc2161e6
Mariya <pkmariya@gmail.com>
2016-10-28 21:22:54 +0530

    commented db configs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
+// var Pool = require('pg').Pool;
 
 var config = {
   user: 'pkmariya', 
@@ -109,7 +109,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Pool(config);
+// var pool = new Pool(config);
 app.get('/test-db', function(req, res) 
 {
    pool.query('SELECT * FROM test', function(err, result)
