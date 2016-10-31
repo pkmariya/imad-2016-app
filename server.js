6e374d9339196111e1762c909568f69c855a9dd3
Mariya <pkmariya@gmail.com>
2016-10-31 16:29:58 +0530

    typed changes

--- server.js
+++ server.js
@@ -136,11 +136,17 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/typed.min.js', function(req, res) {
+	res.sendFile(path.join(__dirname, 'ui', 'typed.min.js'));
+});
+
+app.get('/typed.min.js', function (req, res) {
+	res.sendFile(path.join(__dirname, 'ui', 'typed.min.js'));
+});
 
 app.get('/ui/favicon.ico', function(req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
 });
-
 
 app.get('/:articleName', function(req, res) {
     var articleName = req.params.articleName;
